daa538bf23ef0e8159ecb42784a1be635a78f3be
scttymn <scotty@hey.com>
2024-03-08 23:04:47 -0600

    updating config

--- chadrc.lua
+++ chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "tomorrow_night",
-  theme_toggle = { "tomorrow_night", "one_light" },
+  theme = "ayu_dark",
+  theme_toggle = { "ayu_dark", "one_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
